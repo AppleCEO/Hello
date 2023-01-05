56acb70c82974909cc6a578f532e6a54e926c6d1
joonhokil <kiljh@me.com>
2023-01-05 15:24:53 +0900

    Take

--- HelloRxSwift/RxSwiftPlayground.playground/Contents.swift
+++ HelloRxSwift/RxSwiftPlayground.playground/Contents.swift
@@ -4,17 +4,30 @@ import RxCocoa
 
 let disposeBag = DisposeBag()
 
-let subject = PublishSubject<String>()
-let trigger = PublishSubject<String>()
-
-subject.skipUntil(trigger)
+Observable.of(1,2,3,4,5,6)
+    .take(3)
     .subscribe(onNext: {
         print($0)
     }).disposed(by: disposeBag)
 
-subject.onNext("A")
-subject.onNext("B")
+Observable.of(2,4,6,7,8,10)
+    .takeWhile {
+        return $0 % 2 == 0
+    }.subscribe(onNext: {
+        print($0)
+    }).disposed(by: disposeBag)
+
+let subject = PublishSubject<String>()
+let trigger = PublishSubject<String>()
+
+subject.takeUntil(trigger)
+    .subscribe(onNext: {
+        print($0)
+    }).disposed(by: disposeBag)
+
+subject.onNext("1")
+subject.onNext("2")
 
 trigger.onNext("X")
 
-subject.onNext("C")
+subject.onNext("3")
