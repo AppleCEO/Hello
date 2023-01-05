32f2d14f80bde242a4f9cb67cd4d7fd4cade8879
joonhokil <kiljh@me.com>
2023-01-05 17:47:45 +0900

    toArray

--- HelloRxSwift/RxSwiftPlayground.playground/Contents.swift
+++ HelloRxSwift/RxSwiftPlayground.playground/Contents.swift
@@ -4,30 +4,8 @@ import RxCocoa
 
 let disposeBag = DisposeBag()
 
-Observable.of(1,2,3,4,5,6)
-    .take(3)
+Observable.of(1,2,3,4,5)
+    .toArray()
     .subscribe(onNext: {
         print($0)
     }).disposed(by: disposeBag)
-
-Observable.of(2,4,6,7,8,10)
-    .takeWhile {
-        return $0 % 2 == 0
-    }.subscribe(onNext: {
-        print($0)
-    }).disposed(by: disposeBag)
-
-let subject = PublishSubject<String>()
-let trigger = PublishSubject<String>()
-
-subject.takeUntil(trigger)
-    .subscribe(onNext: {
-        print($0)
-    }).disposed(by: disposeBag)
-
-subject.onNext("1")
-subject.onNext("2")
-
-trigger.onNext("X")
-
-subject.onNext("3")
