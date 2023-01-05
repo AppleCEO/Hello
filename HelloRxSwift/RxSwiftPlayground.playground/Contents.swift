f52839a788b6588d59f719c290f8c0df5bb09bb2
joonhokil <kiljh@me.com>
2023-01-05 19:29:02 +0900

    flatMap

--- HelloRxSwift/RxSwiftPlayground.playground/Contents.swift
+++ HelloRxSwift/RxSwiftPlayground.playground/Contents.swift
@@ -4,8 +4,27 @@ import RxCocoa
 
 let disposeBag = DisposeBag()
 
-Observable.of(1,2,3,4,5)
-    .toArray()
+struct Student {
+    var score: BehaviorRelay<Int>
+}
+
+let john = Student(score: BehaviorRelay(value: 75))
+let mary = Student(score: BehaviorRelay(value: 95))
+
+let student = PublishSubject<Student>()
+
+student.asObserver()
+    .flatMap {
+        $0.score.asObservable()
+    }
     .subscribe(onNext: {
         print($0)
     }).disposed(by: disposeBag)
+
+student.onNext(john)
+john.score.accept(100)
+
+student.onNext(mary)
+mary.score.accept(80)
+
+john.score.accept(43)
