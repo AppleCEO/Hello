bb0eecfc0c5edd55d5d66b3a9846d882792ef700
joonhokil <kiljh@me.com>
2023-01-05 10:38:56 +0900

    Variables

--- HelloRxSwift/RxSwiftPlayground.playground/Contents.swift
+++ HelloRxSwift/RxSwiftPlayground.playground/Contents.swift
@@ -3,21 +3,13 @@ import RxSwift
 
 let disposeBag = DisposeBag()
 
-let subject = ReplaySubject<String>.create(bufferSize: 2)
+let variable = Variable([String]())
 
-subject.onNext("Issue 1")
-subject.onNext("Issue 2")
-subject.onNext("Issue 3")
+variable.value.append("Item 1")
 
-subject.subscribe {
-    print($0)
-}
+variable.asObservable()
+    .subscribe {
+        print($0)
+    }
 
-subject.onNext("Issue 4")
-subject.onNext("Issue 5")
-subject.onNext("Issue 6")
-
-print("[Subscription 2]")
-subject.subscribe {
-    print($0)
-}
+variable.value.append("Item 2")
