44918d05cb437fb300eebe658c8475425b64aefa
joonhokil <kiljh@me.com>
2023-01-11 16:29:43 +0900

    concat

--- HelloRxSwift/RxSwiftPlayground.playground/Contents.swift
+++ HelloRxSwift/RxSwiftPlayground.playground/Contents.swift
@@ -4,9 +4,11 @@ import RxCocoa
 
 let disposeBag = DisposeBag()
 
-let numbers = Observable.of(2,3,4)
+let first = Observable.of(1,2,3)
+let second = Observable.of(4,5,6)
 
-let observable = numbers.startWith(1)
+let observable = Observable.concat([first,second])
+
 observable.subscribe(onNext: {
     print($0)
-}).disposed(by: disposeBag)
+})
